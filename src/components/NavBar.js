6ffd57e6bc3a2205ea9f5aaf68b6b59f81450429
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-06-08 16:50:54 +0300

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,18 @@
 import React from "react";
+// The <NavBar> component should return the following JSX elements:
 
+// a <nav> element
+// inside the nav element, there should be two child elements:
+// an <a> element, with the href attribute set to #home, and the content of "Home"
+// an <a> element, with the href attribute set to #about, and the content of "About"
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+    <nav>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+    </nav>
+  );
 }
 
 export default NavBar;
